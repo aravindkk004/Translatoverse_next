3e99fbe6624c6f46d23d8c3cfcb5794a3931dfe9
aravindkk004 <aravind463kk30@gmail.com>
2024-11-09 16:37:20 +0530

    middleware

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -12,13 +12,22 @@
 // };
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in*",
+  "/sign-up*",
+]);
 
 export default clerkMiddleware(async (auth, request) => {
+  console.log("Incoming request to:", request.url);
   if (!isPublicRoute(request)) {
     await auth.protect();
+    console.log("Route protected, user must be authenticated.");
+  } else {
+    console.log("Public route, no authentication required.");
   }
 });
+
 
 export const config = {
   matcher: [
