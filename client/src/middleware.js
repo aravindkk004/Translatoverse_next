a38d937af0f9a22d390a579be24180fdc0272a85
aravindkk004 <aravind463kk30@gmail.com>
2024-11-09 21:24:33 +0530

    prev

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -11,30 +11,30 @@
 //   ],
 // };
 
-import { authMiddleware } from "@clerk/nextjs";  
+// import { authMiddleware } from "@clerk/nextjs";  
 
-export default authMiddleware({   
-    publicRoutes: ['/', '/app/api/webhooks/clerk/route.js'] });  
+// export default authMiddleware({   
+//     publicRoutes: ['/', '/app/api/webhooks/clerk/route.js'] });  
 
-export const config = {   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"], };
+// export const config = {   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"], };
 
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
-// export default clerkMiddleware(async (auth, request) => {
-//   if (!isPublicRoute(request)) {
-//     await auth.protect();
-//   }
-// });
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
 
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-//     // Always run for API routes
-//     "/(api|trpc)(.*)",
-//   ],
-// };
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
 
 
 // import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
