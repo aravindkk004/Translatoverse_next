fdc57016988ccbee09f44f1135137667d954f0e3
aravindkk004 <aravind463kk30@gmail.com>
2024-11-09 21:47:06 +0530

    git

--- client/src/libs/connectToDb.js
+++ client/src/libs/connectToDb.js
@@ -1,16 +1,31 @@
 import mongoose from "mongoose";
 
-const connection = {};
+const MONGODB_URL = process.env.MONGO_URI;
 
-export const connectToDb = async () => {
-  try {
-    if (connection.isConnected) {
-      console.log("using existing connection");
-      return;
-    }
-    const db = await mongoose.connect(process.env.MONGO_URI);
-    connection.isConnected = db.connections[0].readyState;
-  } catch (error) {
-    console.log(error);
+if (!MONGODB_URL) {
+  throw new Error("Please add MONGODB_URL to your environment variables.");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connect = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL, {
+      dbName: "translatoverse",
+      bufferCommands: false,
+      connectTimeoutMS: 30000,
+    });
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
